3cb04ddd19514d50178336d77ec700999e9f3c6f
撒旦 <12163687@qq.com>
2016-06-26 21:50:20 +0800

    创建

---
+++ ntyjapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ntyjapp
+//
+//  Created by 程嘉雯 on 16/6/26.
+//  Copyright © 2016年 YXG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
